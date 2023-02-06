2236936e4fe8b926d072364b4ab3bb215376fe2e
BluSkreen <jasonduran303@gmail.com>
2023-02-06 13:52:59 -0700

    t3 stack

--- the-buffalo-project/src/apollo-client.js
+++ the-buffalo-project/src/apollo-client.js
@@ -1,8 +1,30 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
 
+import { setContext } from "@apollo/client/link/context";
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("id_token");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 const client = new ApolloClient({
-    uri: "https://countries.trevorblades.com",
-    cache: new InMemoryCache(),
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
 });
 
 export default client;
